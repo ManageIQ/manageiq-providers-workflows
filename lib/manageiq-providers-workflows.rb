99c83b0b11180e638c2061082d393b29b6799d89
Joe Rafaniello <jrafanie@gmail.com>
2023-04-26 18:51:38 -0400

    Drop lib/gem-name.rb in favor of lib/gem/name.rb
    Adopt convention already supported by bundler and required by zeitwerk so we
    don't need to tell zeitwerk to ignore this lib/gem-name.rb file.

--- lib/manageiq-providers-workflows.rb
+++
@@ -1,4 +0,0 @@
-require "manageiq/providers/workflows/engine"
-require "manageiq/providers/workflows/version"
-
-require "manageiq/providers/workflows"
--- lib/manageiq/providers/workflows.rb
+++ lib/manageiq/providers/workflows.rb
@@ -1,3 +1,6 @@
+require "manageiq/providers/workflows/engine"
+require "manageiq/providers/workflows/version"
+
 module ManageIQ
   module Providers
     module Workflows
