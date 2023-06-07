7d3e31b21f9a4703a18c4a2ef6c5406d8cf4ba2e
Adam Grare <adam@grare.com>
2023-06-07 09:31:05 -0400

    Allow creation of Credentials for Workflows

--- app/models/manageiq/providers/workflows/automation_manager/credential.rb
+++ app/models/manageiq/providers/workflows/automation_manager/credential.rb
@@ -1,4 +1,10 @@
 class ManageIQ::Providers::Workflows::AutomationManager::Credential < ManageIQ::Providers::EmbeddedAutomationManager::Authentication
   validates :ems_ref, :presence => true, :uniqueness_when_changed => {:scope => [:tenant_id]},
             :format => {:with => /\A[\w\-]+\z/i, :message => N_("may contain only alphanumeric and _ - characters")}
+
+  FRIENDLY_NAME = "Workflows Credential"
+
+  def self.params_to_attributes(params)
+    params
+  end
 end
