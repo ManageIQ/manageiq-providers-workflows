a1fb21d513235a34962b2647bc6bb15a069c69b2
Adam Grare <adam@grare.com>
2023-06-23 13:53:09 -0400

    Fix parent class for Workflows ScmCredential

--- app/models/manageiq/providers/workflows/automation_manager/scm_credential.rb
+++ app/models/manageiq/providers/workflows/automation_manager/scm_credential.rb
@@ -1,3 +1,5 @@
-class ManageIQ::Providers::Workflows::AutomationManager::ScmCredential < ManageIQ::Providers::EmbeddedAutomationManager::ScmCredential
+class ManageIQ::Providers::Workflows::AutomationManager::ScmCredential < ManageIQ::Providers::Workflows::AutomationManager::Authentication
+  include ManageIQ::Providers::EmbeddedAutomationManager::ScmCredentialMixin
+
   FRIENDLY_NAME = "Workflows SCM Credential".freeze
 end
