e21a848f9c2f8d70a27a8ec393be63e9369d3b32
Adam Grare <adam@grare.com>
2023-07-07 14:36:15 -0400

    Fix ScmCredential Parent Class
    When switching from inheriting from EmbeddedAutomationManager
    Authentication to Workflow Credential the parent class was incorrect

--- app/models/manageiq/providers/workflows/automation_manager/scm_credential.rb
+++ app/models/manageiq/providers/workflows/automation_manager/scm_credential.rb
@@ -1,4 +1,4 @@
-class ManageIQ::Providers::Workflows::AutomationManager::ScmCredential < ManageIQ::Providers::Workflows::AutomationManager::Authentication
+class ManageIQ::Providers::Workflows::AutomationManager::ScmCredential < ManageIQ::Providers::Workflows::AutomationManager::Credential
   include ManageIQ::Providers::EmbeddedAutomationManager::ScmCredentialMixin
 
   FRIENDLY_NAME = "Workflows SCM Credential".freeze
