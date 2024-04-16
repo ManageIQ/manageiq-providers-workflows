833452f0d2fb507fd1350f2b19b9d66a4d9b73d1
Nasar Khan <nasarak98@gmail.com>
2024-04-16 12:01:08 -0400

    add credential type

--- app/models/manageiq/providers/workflows/automation_manager/credential.rb
+++ app/models/manageiq/providers/workflows/automation_manager/credential.rb
@@ -1,2 +1,5 @@
 class ManageIQ::Providers::Workflows::AutomationManager::Credential < ManageIQ::Providers::EmbeddedAutomationManager::Authentication
+  def self.credential_type
+    "workflows_credential_types"
+  end
 end
