5b64ba9c2e21a1758deb8576d55c61e941f84508
Adam Grare <adam@grare.com>
2024-05-07 11:15:05 -0400

    Add task output for failed tasks

--- lib/manageiq/providers/workflows/builtin_methods.rb
+++ lib/manageiq/providers/workflows/builtin_methods.rb
@@ -19,7 +19,12 @@ module ManageIQ
           return error_payload(:cause => "Unable to find MiqTask id: [#{runner_context["miq_task_id"]}]") if miq_task.nil?
 
           runner_context["running"] = miq_task.state != ::MiqTask::STATE_FINISHED
-          runner_context["success"] = miq_task.status == ::MiqTask::STATUS_OK unless runner_context["running"]
+
+          unless runner_context["running"]
+            runner_context["success"] = miq_task.status == ::MiqTask::STATUS_OK
+            runner_context["output"]  = runner_context["success"] ? miq_task.message : {"Error" => "States.TaskFailed", "Cause" => miq_task.message}
+          end
+
           runner_context
         end
 
