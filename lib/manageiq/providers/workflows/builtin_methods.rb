6922aa7e72c96a013c393722c2291e09a3b7d764
Keenan Brock <keenan@thebrocks.net>
2024-07-16 13:04:35 -0400

    Pass user and email into workflows

--- lib/manageiq/providers/workflows/builtin_methods.rb
+++ lib/manageiq/providers/workflows/builtin_methods.rb
@@ -2,9 +2,10 @@ module ManageIQ
   module Providers
     module Workflows
       class BuiltinMethods < BasicObject
-        def self.email(params, _secrets, _context)
+        def self.email(params, _secrets, context)
           options = params.slice("To", "From", "Subject", "Cc", "Bcc", "Body", "Attachment").transform_keys { |k| k.downcase.to_sym }
           options[:from] ||= ::Settings.smtp.from
+          options[:to]   ||= context.execution["_requester_email"]
           miq_task = ::GenericMailer.deliver_task(:generic_notification, options)
 
           {"miq_task_id" => miq_task.id}
