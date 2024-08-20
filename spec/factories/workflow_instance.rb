44b40ef59ecc5d7cb733d974490ddc813d40c60b
Adam Grare <adam@grare.com>
2024-08-20 14:28:03 -0400

    Use WorkflowInstance#manager_ref and execution_id for events

--- spec/factories/workflow_instance.rb
+++ spec/factories/workflow_instance.rb
@@ -1,3 +1,5 @@
 FactoryBot.define do
-  factory :workflows_automation_workflow_instance, :class => "ManageIQ::Providers::Workflows::AutomationManager::Workflow_instance", :parent => :workflow_instance
+  factory :workflows_automation_workflow_instance, :class => "ManageIQ::Providers::Workflows::AutomationManager::Workflow_instance", :parent => :workflow_instance do
+    manager_ref { SecureRandom.uuid }
+  end
 end
