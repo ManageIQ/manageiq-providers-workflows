ec335fdc23d4e9bf372f1b8443a1277aa652fb46
Keenan Brock <keenan@thebrocks.net>
2024-10-16 18:05:21 -0400

    Remove non-existing parent factory references
    These factories were added: https://github.com/ManageIQ/manageiq/pull/22316
    But were removed: https://github.com/ManageIQ/manageiq/pull/22503
    
    They were basically NO-OP factories, so removing is no big deal
    
    For some reason, older versions of FactoryBot were ok with a bad reference
    (probably because the klass was overridden anyway)
    But newer versions of FactoryBot complained

--- spec/factories/workflow_instance.rb
+++ spec/factories/workflow_instance.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
-  factory :workflows_automation_workflow_instance, :class => "ManageIQ::Providers::Workflows::AutomationManager::Workflow_instance", :parent => :workflow_instance do
+  factory :workflows_automation_workflow_instance, :class => "ManageIQ::Providers::Workflows::AutomationManager::Workflow_instance" do
+    status { "pending" }
     manager_ref { SecureRandom.uuid }
   end
 end
