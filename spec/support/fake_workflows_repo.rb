4d19b82663f9ebc9fa00ed4bfb9fea12570fead6
Jason Frey <fryguy9@gmail.com>
2025-01-14 14:18:01 -0500

    Merge pull request #124 from agrare/skip_invalid_workflows_during_sync
    Skip invalid workflows during sync
    
    (cherry picked from commit 0606398d9b5f77c49792ad7c5a19d712e2ff28eb)

--- spec/support/fake_workflows_repo.rb
+++ spec/support/fake_workflows_repo.rb
@@ -12,10 +12,21 @@ module Spec
         dummy_workflow_data_for(full_path) if full_path.fnmatch?("**/*.asl", File::FNM_EXTGLOB)
       end
 
-      def dummy_workflow_data_for(_filename)
-        <<~WORKFLOW_DATA
-          {"Comment": "hello world", "States": {"Start": {"Type": "Succeed"}}, "StartAt": "Start"}
-        WORKFLOW_DATA
+      def dummy_workflow_data_for(filename)
+        case filename.to_s
+        when /invalid_json.asl$/
+          <<~WORKFLOW_DATA
+            {"Invalid Json"
+          WORKFLOW_DATA
+        when /missing_states.asl$/
+          <<~WORKFLOW_DATA
+            {"Comment": "Missing States"}
+          WORKFLOW_DATA
+        else
+          <<~WORKFLOW_DATA
+            {"Comment": "hello world", "States": {"Start": {"Type": "Succeed"}}, "StartAt": "Start"}
+          WORKFLOW_DATA
+        end
       end
     end
   end
